7e54897fe34e38eef7430d41161ba2a83e977567
Hemant Kumar <gethemant@gmail.com>
2014-08-02 14:56:49 -0400

    Remove inheretence from GCstats

--- rbkit-lib/gcstat.cpp
+++ rbkit-lib/gcstat.cpp
@@ -1,12 +1,12 @@
 #include "gcstat.h"
 
-GcStat::GcStat(std::map<std::string, long> heapStat, double timestamp, const QString& eventName)
+GcStat::GcStat(std::map<std::string, long> heapStat, double timestamp, const QString eventName)
 {
     this->heapStat = heapStat;
     this->timestamp = timestamp;
     this->eventName = eventName;
 }
 
-GcStat::GcStat(std::string& eventName, MapStrMsgPackObj& parsedMap) {
+GcStat::GcStat(std::string eventName, MapStrMsgPackObj& parsedMap) {
 
 }
--- rbkit-lib/gcstat.h
+++ rbkit-lib/gcstat.h
@@ -7,12 +7,14 @@
 #include "eventdata.h"
 #include "subscriber.h"
 
-class GcStat : public EventData
+class GcStat
 {
+    double timestamp;
+    QString eventName;
     std::map<std::string, long> heapStat;
 public:
-    GcStat(std::map<std::string, long> dataMap, double timestamp, const QString& eventName);
-    GcStat(std::string& eventName, MapStrMsgPackObj& parsedMap);
+    GcStat(std::map<std::string, long> dataMap, double timestamp, const QString eventName);
+    GcStat(std::string eventName, MapStrMsgPackObj& parsedMap);
 };
 
 #endif // GCSTAT_H
